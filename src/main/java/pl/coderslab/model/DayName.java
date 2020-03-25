01db114e7ba228bd5f247fda04a676336aeea3fb
Pawel <pawmoc77@gmail.com>
2020-03-25 20:05:44 +0100

    Gettery dodane

--- src/main/java/pl/coderslab/model/DayName.java
+++ src/main/java/pl/coderslab/model/DayName.java
@@ -14,9 +14,15 @@ public class DayName {
     public DayName() {
     }
 
+    public int getId() { return id; }
+
     public void setId(int id) { this.id = id; }
 
+    public String getName() { return name; }
+
     public void setName(String name) { this.name = name; }
+
+    public int getDisplayOrder() { return displayOrder; }
 
     public void setDisplayOrder(int displayOrder) { this.displayOrder = displayOrder; }
 }
