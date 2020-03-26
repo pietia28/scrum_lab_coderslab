08e81a8ceacc4acb9838085674e83d6cea56124c
Piotr Głowacki <pege28@wp.pl>
2020-03-26 23:20:02 +0100

    Register.java i register.jsp

--- src/main/java/pl/coderslab/web/Register.java
+++ src/main/java/pl/coderslab/web/Register.java
@@ -1,19 +1,51 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet(name = "Register")
+@WebServlet(name = "Register", urlPatterns={"/register"})
 public class Register extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        if(comparePasswords(request.getParameter("password"),request.getParameter("repassword"))) {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", request.getParameter("name"));
+            params.put("surname", request.getParameter("surname"));
+            params.put("email", request.getParameter("email"));
+            params.put("password", request.getParameter("password"));
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(saveData(params));
+        } else {
+            getServletContext().getRequestDispatcher("/register.jsp")
+                    .forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/register.jsp")
+                .forward(request, response);
+    }
 
+    private boolean saveData(Map params) {
+        AdminDao adminDao = new AdminDao();
+        Admin admin = new Admin();
+        admin.setFirst_name((String) params.get("name"));
+        admin.setLast_name((String) params.get("surname"));
+        admin.setEmail((String) params.get("email"));
+        admin.setPassword((String) params.get("password"));
+        return adminDao.create(admin) != null;
+    }
+
+    private boolean comparePasswords(String password, String repassword) {
+        return password.equals(repassword);
     }
 }
