871c9b1be5b97da094fb3e3c899a0f8baa216c99
Marcin <m.borucki@onet.pl>
2020-03-28 08:56:55 +0100

    przywrocenie wersji sampleSelect.sql

--- src/main/resources/sql/sampleSelect.sql
+++ src/main/resources/sql/sampleSelect.sql
@@ -7,7 +7,7 @@ ORDER by day_name.display_order, recipe_plan.display_order;
 
 
 -- SQL - pobiera najnowszy plan dla zadanego użytkownika (tabela admins)
-SELECT day_name.name as day_name, meal_name,  recipe.name as recipe_name, recipe.id as recipe_id
+SELECT day_name.name as day_name, meal_name,  recipe.name as recipe_name, recipe.description as recipe_description
 FROM `recipe_plan`
 JOIN day_name on day_name.id=day_name_id
 JOIN recipe on recipe.id=recipe_id WHERE
